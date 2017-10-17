558399bbb5aebfe19348a7fb8132714fbc8de32a
Rob Wilson <roobert@gmail.com>
2017-10-17 17:39:28 +0100

    fix tests

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: test build-server
+all: build-server
 
 clean:
 	@rm -v dms
--- db/db_test.go
+++ db/db_test.go
@@ -22,44 +22,51 @@ func TestCreateDB(t *testing.T) {
 
 		CreateDB("test.db")
 
+		// write to db so file gets created
+		CreateTable("test", "id INTEGER PRIMARY KEY, name TEXT")
+
 		if _, err := os.Stat("test.db"); os.IsNotExist(err) {
 			t.Errorf("failed to create db")
 		}
 	}()
 }
 
-//func TestDeleteDB(t *testing.T) {
-//	func() {
-//		defer func() {
-//			if r := recover(); r != nil {
-//				t.Errorf("panic when trying to delete DB")
-//			}
-//		}()
-//
-//		CreateDB("test.db")
-//
-//		if _, err := os.Stat("test.db"); os.IsNotExist(err) {
-//			t.Errorf("failed to create db")
-//		}
-//
-//		DeleteDB("test.db")
-//
-//		if _, err := os.Stat("test.db"); os.IsExist(err) {
-//			t.Errorf("failed to delete db")
-//		}
-//	}()
-//}
-//
-//func TestCreateTable(t *testing.T) {
-//	func() {
-//		defer func() {
-//			if r := recover(); r != nil {
-//				t.Errorf("panic when trying to create table")
-//			} else {
-//				DB.Exec("DELETE TABLE test")
-//			}
-//		}()
-//
-//		CreateTable("test", "id INTEGER PRIMARY KEY, name TEXT")
-//	}()
-//}
+func TestDeleteDB(t *testing.T) {
+	func() {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("panic when trying to delete DB")
+			}
+		}()
+
+		CreateDB("test.db")
+
+		// write to db so file gets created
+		CreateTable("test", "id INTEGER PRIMARY KEY, name TEXT")
+
+		if _, err := os.Stat("test.db"); os.IsNotExist(err) {
+			t.Errorf("failed to create db")
+		}
+
+		DeleteDB("test.db")
+
+		if _, err := os.Stat("test.db"); os.IsExist(err) {
+			t.Errorf("failed to delete db")
+		}
+	}()
+}
+
+func TestCreateTable(t *testing.T) {
+	func() {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("panic when trying to create table")
+			} else {
+				DeleteDB("test.db")
+			}
+		}()
+
+		CreateDB("test.db")
+		CreateTable("test", "id INTEGER PRIMARY KEY, name TEXT")
+	}()
+}
